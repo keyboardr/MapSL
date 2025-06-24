9f77e0d399a6e06d2cf7cd38a3aa0be7eaf29920
keyboardr <keyboardr@gmail.com>
2025-06-24 03:03:28 -0700

    Add accidentally excluded file

---
+++ buildSrc/src/main/java/dev/keyboardr/build/JvmToolchain.kt
@@ -0,0 +1,8 @@
+
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+
+val Project.javaVersion
+  get() = JavaVersion.VERSION_17
+val Project.jdkVersion
+  get() = javaVersion.ordinal + 1
